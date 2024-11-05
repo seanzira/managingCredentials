618a840f465019570b6ab5d85597a77561776394
seanzira <162341287+seanzira@users.noreply.github.com>
2024-11-05 12:31:12 +0200

    Update Division.js

--- models/Division.js
+++ models/Division.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 // Schema for the Division model
 const DivisionSchema = new mongoose.Schema({
   name: { type: String, required: true, unique: true, minlength: 1 },
-  ou: { type: mongoose.Schema.Types.ObjectId, ref: 'OU', required: true },
+  ou: { type: mongoose.Schema.Types.ObjectId, ref: 'Ou', required: true },
   description: { type: String },
 }, { timestamps: true}
 );
